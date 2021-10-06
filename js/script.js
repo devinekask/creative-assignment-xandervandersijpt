f1d65176988c74c4884e35d46fb8b3002e20e1e5
Xander Van der Sijpt <xander.vandersijpt1@gmail.com>
2021-10-06 11:28:37 +0200

    randomized lamp placement and rotate camera

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.133.1-nP52U8LARkTRhxRcba9x/mode=imports/optimized/three.js';
 import {GLTFLoader} from 'https://cdn.skypack.dev/three@v0.133.1/examples/jsm/loaders/GLTFLoader.js';
+import {OrbitControls} from 'https://cdn.skypack.dev/three@v0.133.1/examples/jsm/controls/OrbitControls.js';
 {
     
     //defining some global variables for our project
     //declaring our scene, camera and renderer to initialize our three.js scene.
     const scene = new THREE.Scene();
     //perspective camera: Field of view, aspect ratio, near and far clipping plane.
-    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight,0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight,0.1, 1000);
+    
     const renderer = new THREE.WebGLRenderer();
+    //Adding controls for our camera to move with an event listener (currently autorotating)
+    const controls = new OrbitControls(camera, renderer.domElement);
+    camera.position.set(0,30,10);
+    controls.target.set(10,30,10);
+    controls.update();
+    controls.autoRotate = true;
 
     const handleWindowResize = () => {
         camera.aspect = window.innerWidth / window.innerHeight;
@@ -17,18 +25,24 @@ import {GLTFLoader} from 'https://cdn.skypack.dev/three@v0.133.1/examples/jsm/lo
 
     }
 
+    //random function to generate a random number between a min and max
+    const random = (min,max) => {
+        return Math.random() * (max - min) + min;
+    }
+
     const animate = () => {
         requestAnimationFrame(animate);
+        controls.update();
         
         renderer.render(scene, camera);
     }
 
     const renderFloor = () => {
         //adding a circular floor to our scene
-        const floorGeometry = new THREE.CircleGeometry( 300, 100);
-        const material = new THREE.MeshLambertMaterial( { color: `#C6CAFC` } );
-        // const loader = new THREE.TextureLoader();
-        // const material = new THREE.MeshLambertMaterial( { map: loader.load(`./assets/circlemarble.png`) } );
+        const floorGeometry = new THREE.CircleGeometry( 330, 100);
+        //const material = new THREE.MeshLambertMaterial( { color: `#C6CAFC` } );
+        const loader = new THREE.TextureLoader();
+        const material = new THREE.MeshLambertMaterial( { map: loader.load(`./assets/marble.png`) } );
         const floor = new THREE.Mesh(floorGeometry, material);
         //rotate the circle to be flat on bottom
         floor.rotation.x = -Math.PI/2;
@@ -39,15 +53,36 @@ import {GLTFLoader} from 'https://cdn.skypack.dev/three@v0.133.1/examples/jsm/lo
 
     const renderHemiLight = () => {
         //adding a hemisphere light to light up the entire scene
-        const hemiLight = new THREE.HemisphereLight(`#ffffff`,`#7E89DD`,0.6);
+        const hemiLight = new THREE.HemisphereLight(`#ffffff`,`#7E89DD`,0.1);
         scene.add(hemiLight);
     }
 
     const renderScenicLights = () => {
-        //Creating 10 lights evenly spaced around the scene
-        const lightsRadius = 150;
+        //Creating 12 lights evenly spaced around the scene: 6 with a smaller radius and 6 with a larger one to make them more different in position
+        let lightsRadius;
         const lightsAmount = 12;
         for(let i=0; i<lightsAmount; i++){
+            //define the radius
+            if(i % 2 === 0) {
+                lightsRadius = 150;
+            } else if(i % 3 === 0) {
+                lightsRadius = 180;
+            } else {
+                lightsRadius = 210;
+            }
+
+            //add a light source to our scene
+            const light = new THREE.PointLight(`#ffffff`, 0.3);
+            //adding a sphere to show the location of the light
+            const sphere = new THREE.SphereGeometry(1.5,16,8);
+            light.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffffff } ) ) );
+            //calculate the coordinates so the lights are placed evenly along circular path, the Y is a randomized value to place the bulbs at different heights
+            const positionX = Math.round(lightsRadius * (Math.cos(i* (2 * Math.PI / lightsAmount ))));
+            const positionY = random(25,65);
+            const positionZ = Math.round(lightsRadius * (Math.sin(i* (2 * Math.PI / lightsAmount ))));
+            light.position.set(positionX, positionY, positionZ);
+            scene.add(light);
+
             //add the blender object lamp underneath
             const loader = new GLTFLoader();
             loader.load(`./assets/sceneLight.glb`, 
@@ -55,22 +90,12 @@ import {GLTFLoader} from 'https://cdn.skypack.dev/three@v0.133.1/examples/jsm/lo
                     console.log(`loaded scene`);
                     const mesh = gltf.scene;
                     mesh.position.set(positionX,0,positionZ);
-                    mesh.scale.set(1.5,1.5,1.5);
+                    //making the vertical scaling random here to match with lightbulb height, using the normal height to calculate the right factor height
+
+                    mesh.scale.set(1.5,1.5*(positionY/25),1.5);
                     scene.add(mesh);
                 }
             );
-            
-            //add a light source to our scene
-            const light = new THREE.PointLight(`#ffffff`, 0.3);
-            //adding a sphere to show the location of the light
-            const sphere = new THREE.SphereGeometry(1.5,16,8);
-            light.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffffff } ) ) );
-            //make the coordinates random so they are placed around the scene
-            //light.position.set(0,20,-40);
-            const positionX = Math.round(lightsRadius * (Math.cos(i* (2 * Math.PI / lightsAmount ))));
-            const positionZ = Math.round(lightsRadius * (Math.sin(i* (2 * Math.PI / lightsAmount ))));
-            light.position.set(positionX, 25, positionZ);
-            scene.add(light);
         }
     }
 
@@ -78,19 +103,17 @@ import {GLTFLoader} from 'https://cdn.skypack.dev/three@v0.133.1/examples/jsm/lo
         //add a resize listener to our window, then redraw the scene to fit.
         window.addEventListener('resize', handleWindowResize);
 
-        
         //set size and background color of renderer
         renderer.setSize(window.innerWidth, window.innerHeight);
         //telling our renderer to render in full retina resolution, to improve visual quality.
-        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setPixelRatio( window.devicePixelRatio);
         //adding our rendering canvas to our HTML document
         document.body.appendChild(renderer.domElement);
-        //moving the camera away so that if renders from a certain position
-        camera.position.set(0,30,10);
 
+        
         //set a fog and background color on our scene
         scene.background = new THREE.Color(`#7E89DD`);
-        scene.fog = new THREE.Fog( `#7E89DD`, 1, 280);
+        scene.fog = new THREE.Fog( `#7E89DD`, 1, 300);
 
         //adding a circular floor to our scene
         renderFloor();
